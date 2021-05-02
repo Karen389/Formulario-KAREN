9d7193664d45d2ddffd491bc6c9ca8391104c412
unknown <karenrivera389@gmail.com>
2021-05-02 16:18:43 -0600

    v1..2

--- enviar_emai.php
+++ enviar_emai.php
@@ -1,42 +1,40 @@
 <?php
-// Multiple recipients
-$to = 'johny@example.com, sally@example.com'; // note the comma
+if (isset($_POST)) {
+    // Multiple recipients
+    $to = $_POST("email"); // note the comma
 
-// Subject
-$subject = 'Birthday Reminders for August';
+    // Subject
+    $subject = 'Registro en la Plataforma';
 
-// Message
-$message = '
+    // Message
+    $message = '
 <html>
 <head>
-  <title>Birthday Reminders for August</title>
+  <title>Registro en la Plataforma</title>
 </head>
 <body>
-  <p>Here are the birthdays upcoming in August!</p>
-  <table>
-    <tr>
-      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
-    </tr>
-    <tr>
-      <td>Johny</td><td>10th</td><td>August</td><td>1970</td>
-    </tr>
-    <tr>
-      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
-    </tr>
-  </table>
+  <h2>Hola, ' . $_POST("firstName") . ' ' . $_POST("lastName") . '</h2>
+  
+  <hr>
+  <hr>
+  <hr>
+
+  <p>Te has registrado en la plataforma de Karen</p>
 </body>
 </html>
 ';
 
-// To send HTML mail, the Content-type header must be set
-$headers[] = 'MIME-Version: 1.0';
-$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+    // To send HTML mail, the Content-type header must be set
+    $headers[] = 'MIME-Version: 1.0';
+    $headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
-// Additional headers
-$headers[] = 'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
-$headers[] = 'From: Birthday Reminder <birthday@example.com>';
-$headers[] = 'Cc: birthdayarchive@example.com';
-$headers[] = 'Bcc: birthdaycheck@example.com';
+    // Additional headers
+    //$headers[] = '';
+    //$headers[] = 'From: Birthday Reminder <birthday@example.com>';
+    //$headers[] = 'Cc: birthdayarchive@example.com';
+    //$headers[] = 'Bcc: birthdaycheck@example.com';
 
-// Mail it
-mail($to, $subject, $message, implode("\r\n", $headers));
+    // Mail it
+    mail($to, $subject, $message,implode("\r\n", $headers));
+}
+
